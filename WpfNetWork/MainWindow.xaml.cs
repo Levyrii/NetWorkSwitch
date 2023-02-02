3fd6808b39b53fa84084d14cea290ae15d81754b
Levyrii <liweixxxx1990@163.com>
2023-02-02 16:29:41 +0800

    Add project files.

---
+++ WpfNetWork/MainWindow.xaml.cs
@@ -0,0 +1,353 @@
+﻿using ManagedNativeWifi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfNetWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly NetList _netWorkList = new();
+        private const int INTERNET_CONNECTION_MODEM = 1;
+        private const int INTERNET_CONNECTION_LAN = 2;
+
+        [DllImport("winInet.dll")]
+        private static extern bool InternetGetConnectedState(ref int dwFlag, int dwReserved);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetNetWorkList();
+            Loaded += MainWindow_Loaded;
+            MouseMove += MainWindow_MouseMove;
+        }
+
+        #region 事件处理
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            int wlanStatus = GetNetConStatus("baidu.com");
+            if (wlanStatus == 3)
+            {
+                btnSwitch.Content = "切换到内网";
+                await DisableNWI(_netWorkList.EthernetNWIName);
+            }
+            else
+            {
+                //如果启动时外网不通一律认为当前时内外（即使不太准确）
+                //int lanStatus = GetNetConStatus("10.20.13.100");
+                btnSwitch.Content = "切换到外网";
+                await DisableNWI(_netWorkList.WirelessNWIName);
+            }
+        }
+
+        private async void BtnSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            ResetNWIName();
+            if (btnSwitch.Content.ToString() == "切换到内网")
+            {
+                btnSwitch.Content = "切换中...";
+                await DisableNWI(_netWorkList.WirelessNWIName);
+                await EnableNWI(_netWorkList.EthernetNWIName);
+                btnSwitch.Content = "切换到外网";
+            }
+            else
+            {
+                btnSwitch.Content = "切换中...";
+                await DisableNWI(_netWorkList.EthernetNWIName);
+                await EnableNWI(_netWorkList.WirelessNWIName);
+                btnSwitch.Content = "Wifi连接中...";
+                await Task.Delay(2000);
+                bool ret = await ConnectWifiAsync(txtWifi.Text);
+                //MessageBox.Show(ex.ToString());
+                btnSwitch.Content = "切换到内网";
+            }
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var pt = e.GetPosition(this);
+                if (pt.X > 0 && pt.X < 200 && pt.Y > 0 && pt.Y < 25)
+                {
+                    this.DragMove();
+                }
+            }
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 设置初始网络集合
+        /// </summary>
+        private void SetNetWorkList()
+        {
+            var validNWILst = GetValidNWI();
+            var wirelessNWI = validNWILst.FirstOrDefault(
+                p => p.NetworkInterfaceType == NetworkInterfaceType.Wireless80211);
+            _netWorkList.Add(new NetCls(wirelessNWI == null ? "WLAN" : wirelessNWI.Name, 0));
+
+            var ethernetNWI = validNWILst.FirstOrDefault(
+                p => p.NetworkInterfaceType == NetworkInterfaceType.Ethernet);
+            string ethernetName = ethernetNWI == null ?
+                (string.IsNullOrEmpty(txtEnther.Text) ? "以太网" : txtEnther.Text) : ethernetNWI.Name;
+
+            _netWorkList.Add(new NetCls(ethernetName, 1));
+        }
+
+        /// <summary>
+        /// 重新设置当前最新的NWI名称
+        /// </summary>
+        private void ResetNWIName()
+        {
+            var ethernetNWI = _netWorkList.EthernetNWI;
+            if (ethernetNWI != null && !string.IsNullOrEmpty(txtEnther.Text))
+            {
+                ethernetNWI.Name = txtEnther.Text;
+            }
+        }
+
+        /// <summary>
+        /// 获取有效的网络接口
+        /// </summary>
+        /// <returns></returns>
+        private List<NetworkInterface> GetValidNWI()
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            var ret = interfaces.Where(p => p.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+            !p.Description.Contains("Virtual") && !p.Description.Contains("VPN"));
+            return ret.ToList();
+        }
+        #endregion
+
+        /// <summary>
+        /// 启用网络Interface
+        /// </summary>
+        /// <param name="interfaceName"></param>
+        private Task EnableNWI(string? interfaceName)
+        {
+            if (string.IsNullOrEmpty(interfaceName)) return Task.CompletedTask;
+            ProcessStartInfo psi = new("netsh", $"interface set interface \"{interfaceName}\" admin=enable")
+            {
+                CreateNoWindow = true
+            };
+            Process p = new Process
+            {
+                StartInfo = psi
+            };
+            p.Start();
+            return p.WaitForExitAsync();
+        }
+
+        /// <summary>
+        /// 禁用网络Interface
+        /// </summary>
+        /// <param name="interfaceName"></param>
+        private Task DisableNWI(string? interfaceName)
+        {
+            if (string.IsNullOrEmpty(interfaceName)) return Task.CompletedTask;
+            ProcessStartInfo psi = new("netsh", $"interface set interface \"{interfaceName}\" admin=disable")
+            {
+                CreateNoWindow = true
+            };
+            Process p = new Process
+            {
+                StartInfo = psi
+            };
+            p.Start();
+            return p.WaitForExitAsync();
+        }
+
+        /// <summary>
+        /// 获取网络连接状态
+        /// </summary>
+        /// <param name="strNetAddress"></param>
+        /// <returns></returns>
+        private int GetNetConStatus(string strNetAddress)
+        {
+            int iNetStatus = 0;
+            int dwFlag = new int();
+            if (!InternetGetConnectedState(ref dwFlag, 0))
+            {
+                //没有能连上互联网
+                iNetStatus = 1;
+            }
+            else if ((dwFlag & INTERNET_CONNECTION_MODEM) != 0)
+            {
+                //采用调治解调器上网,需要进一步判断能否登录具体网站
+                if (PingNetAddress(strNetAddress))
+                {
+                    //可以ping通给定的网址,网络OK
+                    iNetStatus = 2;
+                }
+                else
+                {
+                    //不可以ping通给定的网址,网络不OK
+                    iNetStatus = 4;
+                }
+            }
+
+            else if ((dwFlag & INTERNET_CONNECTION_LAN) != 0)
+            {
+                //采用网卡上网,需要进一步判断能否登录具体网站
+                if (PingNetAddress(strNetAddress))
+                {
+                    //可以ping通给定的网址,网络OK
+                    iNetStatus = 3;
+                }
+                else
+                {
+                    //不可以ping通给定的网址,网络不OK
+                    iNetStatus = 5;
+                }
+            }
+            return iNetStatus;
+        }
+
+        /// <summary>
+        /// ping 具体的网址看能否ping通
+        /// </summary>
+        /// <param name="strNetAdd"></param>
+        /// <returns></returns>
+        private bool PingNetAddress(string strNetAdd)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply pr = ping.Send(strNetAdd, 3000);
+                return pr.Status == IPStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// wifi连接
+        /// </summary>
+        /// <param name="sid"></param>
+        /// <returns></returns>
+        public async Task<bool> ConnectWifiAsync(string sid)
+        {
+            NativeWifi.ThrowsOnAnyFailure = true;
+            var ret = NativeWifi.EnumerateAvailableNetworks().ToList();
+            var availableNetwork = ret
+                .Where(x => x.ProfileName == sid)
+                .OrderByDescending(x => x.SignalQuality)
+                .FirstOrDefault();
+
+            if (availableNetwork is null) return false;
+
+            return await NativeWifi.ConnectNetworkAsync(
+                interfaceId: availableNetwork.Interface.Id,
+                profileName: availableNetwork.ProfileName,
+                bssType: availableNetwork.BssType,
+                timeout: TimeSpan.FromSeconds(10));
+        }
+
+        /// <summary>
+        /// 禁用网卡
+        /// </summary>
+        /// <param name="network"></param>
+        /// <returns></returns>
+        public bool DisableNetWork(ManagementObject network)
+        {
+            if (network == null) return false;
+            try
+            {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                network.InvokeMethod("Disable", null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 启用网卡
+        /// </summary>
+        /// <param name="network"></param>
+        /// <returns></returns>
+        public bool EnableNetWork(ManagementObject network)
+        {
+            if (network == null) return false;
+            try
+            {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                network.InvokeMethod("Enable", null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public ManagementObject? NetWork(string networkname)
+        {
+            string netState = "SELECT * FROM WIN32_NETWORKADAPTER";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(netState);
+            ManagementObjectCollection collection = searcher.Get();
+
+            foreach (ManagementObject manage in collection)
+            {
+                if (manage["Name"].ToString() == networkname)
+                {
+                    return manage;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class NetCls
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 0:无线 1:有线
+        /// </summary>
+        public int Type { get; set; }
+
+        public NetCls(string name, int type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+
+    public class NetList : List<NetCls>
+    {
+        public NetCls? WirelessNWI => this.FirstOrDefault(p => p.Type == 0);
+        public NetCls? EthernetNWI => this.FirstOrDefault(p => p.Type == 1);
+
+        public string? WirelessNWIName => WirelessNWI?.Name;
+        public string? EthernetNWIName => EthernetNWI?.Name;
+    }
+}
---
+++ WpfNetWork/Window2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfNetWork
+{
+    /// <summary>
+    /// Window2.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var testLst = Enumerable
+            //    .Range(0, 10000)
+            //    .Select(p => new TestCls()
+            //    {
+            //        IntProp = p,
+            //        DecimalProp = p * 1.0101M,
+            //        BoolProp = p % 2 == 0,
+            //        DateTimeProp = DateTime.Now.AddSeconds(-p),
+            //        StrProp1 = $"str1{p}",
+            //        StrProp2 = $"str2{p}",
+            //        StrProp3 = $"str3{p}str3",
+            //        StrProp4 = $"{p}str4"
+            //    }).ToList();
+
+            //var str = JsonSerializer.Serialize(testLst, TestContext.Default.ListTestCls);
+        }
+    }
+}
